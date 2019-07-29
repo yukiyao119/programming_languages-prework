bcaea81130867fd305d84646f5c02fd520c35efd
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-29 20:38:53 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,14 +3,12 @@ require 'pry'
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  keys = []
   languages.collect do |feature, feature_data|
     # binding.pry
     feature_data.collect do |language, language_data|
       # binding.pry
       language_data.collect do |type, type_value|
         # binding.pry
-      
       new_hash[language] = {
         type => type_value,
         :style => feature
