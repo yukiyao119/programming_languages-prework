4909955d8469f6db0fb587b1a22a196c8cd72155
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-29 21:44:31 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  # cur_style = []
+  style_arr = languages.keys
   languages.collect do |style, style_data|
     # cur_style[0] = style
     # binding.pry
@@ -11,11 +11,18 @@ def reformat_languages(languages)
       # binding.pry
       language_data.collect do |feature, feature_value|
       # binding.pry
-      new_hash[language] = {
-        feature => feature_value,
-        :style => [style]
-      } 
-      # binding.pry
+        if language == :javascript
+          new_hash[language] = {
+          feature => feature_value,
+          :style => style_arr
+        } 
+        else 
+          new_hash[language] = {
+            feature => feature_value,
+            :style => [style]
+          } 
+          # binding.pry
+        end
       end
     end 
   end
