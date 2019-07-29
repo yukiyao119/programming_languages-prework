a40e901df39129194f1674cf6587bb560a5be10f
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-29 21:16:24 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,7 +6,7 @@ def reformat_languages(languages)
   styles = languages.keys
   # binding.pry
   languages.collect do |style, feature_data|
-    binding.pry
+    # binding.pry
     cur_style[0] = style
     binding.pry
     feature_data.collect do |language, language_data|
