999a6d9c23b4c728d7ffce32bf1c7a0106b1da58
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-29 20:12:02 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,12 @@
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  languages.collect do |key, value|
-    
+  keys = []
+  languages.collect do |feature, feature_data|
+    feature_data.collect do |language, language_data|
+      language_data.collect do |type, type_value|
+        
+      end 
+    end 
   end
 end
