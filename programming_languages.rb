69b8680896d3a2006dd48a65f30f0760d166656f
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-29 20:44:46 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,16 +4,17 @@ def reformat_languages(languages)
   # your code here
   new_hash = {}
   language_arr = []
-  
+  feature_arr = []
   languages.collect do |feature, feature_data|
     # binding.pry
+    feature_arr << feature
     feature_data.collect do |language, language_data|
       # binding.pry
       language_data.collect do |type, type_value|
         # binding.pry
       new_hash[language] = {
         type => type_value,
-        :style => feature
+        :style => feature_arr
       }
       binding.pry
       end 
