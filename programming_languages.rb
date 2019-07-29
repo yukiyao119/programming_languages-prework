4756f177b4680805c6f9a2a53134277e3e472982
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-29 20:58:55 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,8 +5,10 @@ def reformat_languages(languages)
   new_hash = {}
   styles = languages.keys
   # binding.pry
+  cur_style = []
   languages.collect do |style, feature_data|
     binding.pry
+    cur_style << style
     feature_data.collect do |language, language_data|
       # binding.pry
       language_data.collect do |type, type_value|
@@ -14,7 +16,7 @@ def reformat_languages(languages)
       feature_arr << feature
       new_hash[language] = {
         type => type_value,
-        :style => style
+        :style => cur_style.last
       }
       # binding.pry
       end 
