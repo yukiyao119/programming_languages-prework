88f4cccdd42c105ed0e82ee31a323ded415df79f
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-29 21:21:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,11 +3,10 @@ require 'pry'
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  styles = languages.keys
-  cur_style = []
   # binding.pry
   languages.collect do |style, style_data|
     # binding.pry
+    cur_style = []
     style_data.collect do |language, language_data|
       language_data.collect do |feature, feature_value|
       # binding.pry
